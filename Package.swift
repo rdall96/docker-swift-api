94fbd1e058b45aa52e75fdc1cd7280604aca0b50
Ricky Dall'Armellina <rickydallarmallina@gmail.com>
2025-11-16 21:55:01 -0500

    Fix dependency versions

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.30.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", .upToNextMinor(from: "2.86.2")),
+        .package(url: "https://github.com/swift-server/async-http-client.git", .upToNextMinor(from: "1.29.1")),
     ],
     targets: [
         .target(
