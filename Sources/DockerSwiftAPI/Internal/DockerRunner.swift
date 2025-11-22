4acebc9be653f4ac3981829171fcd3362502b228
Ricky Dall'Armellina <rickydallarmallina@gmail.com>
2025-11-22 18:11:36 -0500

    Add a debug log before firing a Docker request

--- Sources/DockerSwiftAPI/Internal/DockerRunner.swift
+++ Sources/DockerSwiftAPI/Internal/DockerRunner.swift
@@ -97,6 +97,7 @@ extension DockerRunner {
         // Run the request
         let response: HTTPClient.Response
         do {
+            logger.debug("[\(request.method.rawValue)] \(requestPath)")
             response = try await self.response(
                 for: requestPath,
                 method: request.method.httpMethod,
